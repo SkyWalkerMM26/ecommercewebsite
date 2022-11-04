ef5bc58b2475e3c4016ca317b9ad345817be1d80
SkyWalkerMM26 <myia_moua26@yahoo.com>
2022-11-04 09:52:21 -0700

    Create a try and catch method to update req.body depending on its id value

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -45,8 +45,20 @@ router.post('/', async (req, res) => {
   }
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a category by its `id` value
+  try {
+    const categoryInfo = await Category.update(req.body, {
+      where: { id: req.params.id }
+    });
+    if (!categoryInfo) {
+      res.status(404).json({ message: 'This category id does not exist' });
+      return;
+    }
+    res.status(200).json(categoryInfo);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.delete('/:id', (req, res) => {
