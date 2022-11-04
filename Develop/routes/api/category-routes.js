f0b970f592b36f5a9090c8be3ea3976ed94d8654
SkyWalkerMM26 <myia_moua26@yahoo.com>
2022-11-04 10:00:34 -0700

    create a try and catch method to delete a category by its id

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -61,8 +61,20 @@ router.put('/:id', async (req, res) => {
   }
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete a category by its `id` value
+  try {
+    const categoryInfo = await Category.destroy({
+      where: { id: req.params.id }
+    });
+    if (!categoryInfo) {
+      res.status(404).json({ message: 'This category id does not exist' });
+      return;
+    }
+    res.status(200).json(categoryInfo);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 module.exports = router;
