fb4b49aad3134b7363612e027bc10d2e2b1d1836
SkyWalkerMM26 <myia_moua26@yahoo.com>
2022-11-04 22:29:36 -0700

    create try and catch method for /:id route to find tag by its enter id

--- Develop/routes/api/tag-routes.js
+++ Develop/routes/api/tag-routes.js
@@ -21,7 +21,20 @@ router.get('/', async (req, res) => {
 router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
-
+  try {
+    const tagInfo = await Tag.findByPk(req.params.id, {
+      include: [{ model: Product, through: ProductTag,
+        attributes: ['id', 'product_name', 'price', 'stock'],
+      }]
+    });
+    if (!tagInfo) {
+      res.status(404).json({ message: 'No category found with this id!' });
+      return;
+    }
+    res.status(200).json(tagInfo);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
